2bc7762c11b24d0a5a8d04849f69377c0c9dc476
Jeff Cadieux <jeffcadieux@hotmail.com>
2020-11-20 19:09:39 +0900

    Add note handleChange function works

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CreateArea() {
+
+    const [noteText, setNoteText] = useState({ title: "", content: "" })
+
+    function handleChange(event) {
+        const { name, value } = event.target
+        switch (name) {
+            case "title":
+                setNoteText(Object.assign({}, noteText, { title: value }))
+                break
+            case "content":
+                setNoteText(Object.assign({}, noteText, { content: value }))
+                break
+            default:
+                console.log("Error: element name is not recognised")
+        }
+    }
+
     return <div>
         <form>
-            <input name="title" placeholder="Title" />
-            <textarea name="content" placeholder="Take a note..." rows="3" />
+            <input name="title" placeholder="Title" onChange={handleChange} value={noteText.title} />
+            <textarea name="content" placeholder="Take a note..." rows="3" onChange={handleChange} value={noteText.content} />
             <button>Add</button>
         </form>
     </div>
