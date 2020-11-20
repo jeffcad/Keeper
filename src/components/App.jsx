1c3952abb13a20c1e4e2a930ce99d5fa839c7bf5
Jeff Cadieux <jeffcadieux@hotmail.com>
2020-11-20 18:57:59 +0900

    Final stage - starter code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,18 +2,13 @@ import React from 'react'
 import { Header } from './Header'
 import { Note } from './Note'
 import { Footer } from './Footer'
-import { notes } from '../notes'
+import { CreateArea } from './CreateArea'
 
 function App() {
     return <div>
         <Header />
-        {notes.map(note =>
-            <Note
-                key={note.key}
-                title={note.title}
-                content={note.content}
-            />
-        )}
+        <CreateArea />
+        <Note key={1} title="Note title" content="Note content" />
         <Footer />
     </div>
 }
